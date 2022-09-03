6f79e808b282e30242a1fa58a5e19e3880c38f95
krkulaot <krkulaot@tktk.ee>
2022-09-03 14:55:24 +0200

    create new app

---
+++ MinuEsimeneApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is my comment 
+
+//Rakendus küsib kasutajalt nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Sisesta oma nimi:");//output
+//string-sõne
+string userName = Console.ReadLine();//input
+
+Console.WriteLine("Tere," +" "+ userName +"!");//output
+Console.WriteLine($"Tere, {userName}!");//output
+
